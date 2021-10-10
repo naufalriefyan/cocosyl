0a1e02521124387c993e971340419a2ac1b18769
Naufal Ahmad Riefyan <58657931+naufalriefyan@users.noreply.github.com>
2021-10-10 21:59:59 +0700

    Add files via upload

---
+++ cocosyl/index.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+
+    <!-- link css -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Zilla+Slab:ital@1&display=swap" rel="stylesheet">
+
+
+    <!-- Icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+
+    <!-- Slider JS -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
+    <title>Pusat pengolahan Kelapa Terbaik di Tasikmalaya | CocoSyl</title>
+  </head>
+  <body>
+
+  <nav class="navbar navbar-dark navbar-expand-lg">
+    <a class="navbar-brand" href="#">
+      <img src="asset/logo.png" class="img-fluid" alt="">
+    </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvasLg" aria-controls="navbarOffcanvasLg">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item nav-page">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item nav-page">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item nav-page">
+            <a class="nav-link" href="#">Product</a>
+          </li>
+          <li class="nav-item nav-page">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="fas fa-language"></i> ID
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
+              <li><a class="dropdown-item" href="#">Bahasa Indonesia</a></li>
+              <li><a class="dropdown-item" href="#">English</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<!-- Main Header -->
+
+
+<div class="header">
+  <div class="container container-header">
+    <h1 class="title-header">The Best Coconut Product.</h1>
+    <h4 class="sub-title-header">We process a raw material into a manufactured 
+                                product. We choose the best coconut tree.</h4>
+    <a href="" class="btn btn-outline-light more-info">More Info</a>
+  </div>
+
+  <div class="container container-sub-header">
+    <div class="row">
+      <div class="col pricing">
+        <h5 class="title-info">Competitive Pricing</h5>
+        <p class="sub-title-info">With 500+ suppliers and the purchasing power of 400 million member.</p>
+      </div>
+      
+      <div class="col coverage">
+        <h5 class="title-info">Worldwide Coverage</h5>
+        <p class="sub-title-info">Over 100.000 entrepreneurs & companies subscribe our products from domestic to Internationals.</p>
+      </div>
+
+      <div class="col service">
+        <h5 class="title-info">Best Service & Seller</h5>
+        <p class="sub-title-info">We’ll do the best service because customer convenience is our priority & our product is the best sellers in Indonesian.</p>
+      </div>
+    </div>
+  </div>
+  <img src="asset/bg.png" class="background" alt="">
+</div>
+
+
+<!-- End Main-Header -->
+
+
+<section class="about">
+    <div class="container">
+      <div class="row">
+        <div class="col left mt-1">
+          <img src="asset/logo-black.png" alt="">
+          <h3 class="t1">COCO</h3>
+          <h2 class="t2">SYL</h2>
+          <h4 class="t3">UNIVERSAL</h4>
+          <h5 class="about-us">About Us</h5>
+          <p class="info-about">
+          <span>We process more than 1,500 hectares of coconut plantations, spread across several islands in Eastern Indonesia.</span>
+          
+          <span>In the beginning, our company was engaged in manufacturing where we processed coconut derivative products, namely coconut shells into coconut shell charcoal. And after that our company expanded and expanded into coconut plantations, to process more derivative products.</span>
+          </p>
+        </div>
+
+        <div class="col right mt-5">
+          <img src="asset/about-bg.png" class="about-bg" alt="">
+        </div>
+      </div>
+    </div>
+</section>
+
+
+
+<section class="product">
+  <div class="container header">
+    <h4 class="title">Our Product</h4>
+    <h1 class="sub-title">The Best Produce</h1>
+  </div>
+
+  <div class="content">
+
+  </div>
+</section>
+
+
+
+  
+
+  
+  
+
+    <!-- Optional JavaScript; choose one of the two! -->
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.min.js" integrity="sha384-PsUw7Xwds7x08Ew3exXhqzbhuEYmA2xnwc8BuD6SEr+UmEHlX8/MCltYEodzWA4u" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
