fd88c88dc353abad4f5bb65f8df073bd841d7803
Naufal Ahmad Riefyan <58657931+naufalriefyan@users.noreply.github.com>
2021-10-11 21:33:46 +0700

    Add files via upload

--- cocosyl/index.php
+++ cocosyl/index.php
@@ -108,7 +108,7 @@
     <div class="container">
       <div class="row">
         <div class="col left mt-1">
-          <img src="asset/logo-black.png" alt="">
+          <img src="asset/logo-black.png" class="" alt="">
           <h3 class="t1">COCO</h3>
           <h2 class="t2">SYL</h2>
           <h4 class="t3">UNIVERSAL</h4>
@@ -142,6 +142,7 @@
 
 
 
+
   
 
   
@@ -158,5 +159,11 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.min.js" integrity="sha384-PsUw7Xwds7x08Ew3exXhqzbhuEYmA2xnwc8BuD6SEr+UmEHlX8/MCltYEodzWA4u" crossorigin="anonymous"></script>
     -->
+
+
+    <script>
+     
+     
+    </script>
   </body>
 </html>
